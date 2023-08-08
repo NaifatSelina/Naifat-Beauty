56437ea4577c3aca79990c0190710e207d3c2886
NaifatSelina <naifatselina@icloud.com>
2023-08-08 18:53:22 +0000

    add date time formatting for fields

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -32,8 +32,16 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+        'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -54,7 +62,8 @@ SECRET_KEY = 'django-insecure-mn@ahdb2uodsf4u58a*r#o6%dnwoffd5nlqb2d$vz0iqc-rni9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-naifatselina-drfapi-hpoha6v31mo.ws-eu102.gitpod.io', 'yourdomain.com', '127.0.0.1']
+
 
 
 # Application definition
