19849c9cd716b3467792114820ff35d7f5290f92
NaifatSelina <naifatselina@icloud.com>
2023-08-09 12:52:35 +0000

    two environment variables to access deployed API on clientside

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -64,7 +65,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-naifatselina-drfapi-hpoha6v31mo.ws-eu102.gitpod.io', 'yourdomain.com', '127.0.0.1', 'drf-api-app-65d9b6a619df.herokuapp.com']
+ALLOWED_HOSTS = ['8000-naifatselina-drfapi-hpoha6v31mo.ws-eu102.gitpod.io', 'yourdomain.com', '127.0.0.1', 'localhost', os.environ.get('ALLOWED_HOST'),]
 
 
 
@@ -109,14 +110,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-     CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-     ]
-else:
-     CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https://.*\.gitpod\.io$",
-     ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
