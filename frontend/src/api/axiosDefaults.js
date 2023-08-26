44f1c5f00a5f9d0d9179deaa1dcb498a26533848
NaifatSelina <naifatselina@icloud.com>
2023-08-26 09:53:31 +0000

    add proxy key to package.json file and comment out baseURL

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://drf-api-app-65d9b6a619df.herokuapp.com/'
+// axios.defaults.baseURL = 'https://drf-api-app-65d9b6a619df.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 
